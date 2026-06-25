d7a1aeb4e0440641cbfceb14c4efe339bfa2ad43
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-06-25 20:02:22 +0300

    Load user portal frontend styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
 import './shared/styles.css';
 import './shared/rg45.css';
+import './shared/rg82.css';
 
 const queryClient = new QueryClient();
 
