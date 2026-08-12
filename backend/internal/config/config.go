1e236c38507191a7e858ce21bd6c9370db90d0e5
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-08-12 08:47:00 +0300

    RG-112B: configure master key path

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -23,13 +23,13 @@ type TelegramConfig struct {
 }
 
 type WhatsAppConfig struct {
-	AccessToken             string
-	PhoneNumberID           string
-	GraphAPIVersion         string
-	VPNAccessTemplate       string
+	AccessToken               string
+	PhoneNumberID             string
+	GraphAPIVersion           string
+	VPNAccessTemplate         string
 	VPNAccessReissuedTemplate string
-	LanguageEN              string
-	LanguageRU              string
+	LanguageEN                string
+	LanguageRU                string
 }
 
 type Config struct {
@@ -37,6 +37,7 @@ type Config struct {
 	HTTPAddr                  string
 	PublicURL                 string
 	DatabaseURL               string
+	SecretsKeyFile            string
 	LogLevel                  slog.Level
 	AuthSessionTTL            time.Duration
 	BootstrapAdminEmail       string
@@ -54,6 +55,7 @@ func Load() Config {
 		HTTPAddr:                  env("ROUTEGATE_HTTP_ADDR", ":8080"),
 		PublicURL:                 env("ROUTEGATE_PUBLIC_URL", ""),
 		DatabaseURL:               env("ROUTEGATE_DATABASE_URL", "postgres://routegate:routegate_dev_password@localhost:5432/routegate?sslmode=disable"),
+		SecretsKeyFile:            env("ROUTEGATE_MASTER_KEY_FILE", "/etc/routegate/master.key"),
 		LogLevel:                  parseLogLevel(env("ROUTEGATE_LOG_LEVEL", "info")),
 		AuthSessionTTL:            time.Duration(envInt("ROUTEGATE_AUTH_SESSION_TTL_HOURS", 24)) * time.Hour,
 		BootstrapAdminEmail:       env("ROUTEGATE_BOOTSTRAP_ADMIN_EMAIL", ""),
