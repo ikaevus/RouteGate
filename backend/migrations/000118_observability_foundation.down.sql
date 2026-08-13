41fc75d397b616e60b2c6cd289ecd867cfdcd3a4
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-08-14 02:39:19 +0300

    RG-113A add observability rollback migration

---
+++ backend/migrations/000118_observability_foundation.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS observability_alert_acknowledgements;
+DROP TABLE IF EXISTS observability_alert_transitions;
+DROP TABLE IF EXISTS observability_alerts;
+DROP TABLE IF EXISTS observability_events;
+DROP TABLE IF EXISTS observability_health_transitions;
+DROP TABLE IF EXISTS observability_current_health;
