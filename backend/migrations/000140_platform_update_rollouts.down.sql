7600a58f9d0e55f01ae980656a48f43e48b182be
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-08-26 23:52:42 +0300

    RG-96E3a: make rollout persistence rollback symmetric

--- backend/migrations/000140_platform_update_rollouts.down.sql
+++ backend/migrations/000140_platform_update_rollouts.down.sql
@@ -1,2 +1,8 @@
+DROP TRIGGER IF EXISTS trg_platform_update_rollout_entries_transition ON platform_update_rollout_entries;
+DROP FUNCTION IF EXISTS enforce_platform_update_rollout_entry_transition();
+DROP TRIGGER IF EXISTS trg_platform_update_rollouts_transition ON platform_update_rollouts;
+DROP FUNCTION IF EXISTS enforce_platform_update_rollout_transition();
 DROP TABLE IF EXISTS platform_update_rollout_entries;
 DROP TABLE IF EXISTS platform_update_rollouts;
+ALTER TABLE IF EXISTS agent_platform_update_jobs
+    DROP CONSTRAINT IF EXISTS agent_platform_update_jobs_rollout_identity;
