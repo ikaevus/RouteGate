110e61140fe0db09edc7620e570d2a87b4e80f03
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-08-28 05:54:36 +0300

    RG-96E3d: rollback job admission trigger cleanly

--- backend/migrations/000142_platform_update_rollout_history_watermark.down.sql
+++ backend/migrations/000142_platform_update_rollout_history_watermark.down.sql
@@ -1,3 +1,6 @@
+DROP TRIGGER IF EXISTS trg_agent_platform_update_jobs_admission_lock ON agent_platform_update_jobs;
+DROP FUNCTION IF EXISTS lock_platform_update_job_admission();
+
 DROP TRIGGER IF EXISTS trg_platform_update_rollout_entries_transition ON platform_update_rollout_entries;
 DROP FUNCTION IF EXISTS enforce_platform_update_rollout_entry_transition();
 
