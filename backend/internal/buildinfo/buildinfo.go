2068d454d52ad5afa6d5ac41bdebb7086d09de9a
ikaevus <62504580+ikaevus@users.noreply.github.com>
2026-08-29 05:35:56 +0300

    RG-96E3e: advance expected DB schema to 143

--- backend/internal/buildinfo/buildinfo.go
+++ backend/internal/buildinfo/buildinfo.go
@@ -4,7 +4,7 @@ const (
 	AgentProtocolVersion                  = 1
 	MinimumSupportedAgentProtocolVersion = 1
 	RecommendedAgentVersion               = "dev"
-	ExpectedDatabaseSchemaVersion         = 142
+	ExpectedDatabaseSchemaVersion         = 143
 	WebUIVersion                          = "dev"
 	UpdateStatus                          = "manual"
 	UpdateChannel                         = "development"
